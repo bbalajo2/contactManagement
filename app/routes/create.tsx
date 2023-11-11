9b233d5df834d818d64848f829fa04160a7cfe0a
bbalajo2 <56073751+bbalajo2@users.noreply.github.com>
2023-11-11 16:44:34 +0000

    all function implemented crud/Search

--- app/routes/create.tsx
+++ app/routes/create.tsx
@@ -35,7 +35,7 @@ export const action: ActionFunction = async ({ request }) => {
         });
     }
     
-    return redirect('~/routes/index');
+    return redirect('../index');
 }
     
 
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,6 +1,6 @@
-import { Form, useLoaderData } from '@remix-run/react';
+import { redirect } from '@remix-run/node';
+import { Form, useLoaderData, Link } from '@remix-run/react';
 import { db } from '~/services/index';
-import { Link } from '@remix-run/react';
 
 export default function Index() {
     const { contacts } = useLoaderData<{ contacts: contact[] }>();
@@ -9,10 +9,11 @@ export default function Index() {
         <div className="container mx-auto p-4">
             <Link to="/create" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Add</Link>
             <h1 className="text-2xl font-bold text-center mb-6" title='Contacts'>Contacts</h1>
+            <Form method='get'>
+                <input type="text" name="search" placeholder="Search" className="border border-gray-200 rounded shadow p-2" />
+                <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Search</button>
+            </Form>
             <ul className="list-none space-y-4">
-                <Form>
-
-                </Form>
                 {contacts.map((contact: contact) => (
                     <li key={contact.id} className="p-4 border border-gray-200 rounded shadow">
                         <p className="font-medium">Name: {contact.name}</p>
@@ -29,15 +30,33 @@ export default function Index() {
     );
 }
 
+export async function loader( { request } : { request: Request }) {
+    const url = new URL(request.url);
+    const search = url.searchParams.get('search');
+
+    let queryOptions = {};
+    if (search != null) {
+        queryOptions = {
+            where: {
+                OR: [
+                    { name: { contains: search, mode: 'insensitive' } },
+                    { email: { contains: search, mode: 'insensitive' } },
+                    { address: { contains: search, mode: 'insensitive' } },
+                ]
+            }
+        };
+    }else {
+        redirect("./index")
+    }
+
+    const contacts = await db.contacts.findMany(queryOptions);
+    return { contacts };
+}
+
 interface contact {
     id: number;
     name: string;
     email: string;
     phone_number: number;
     address?: string;
-}
-
-export async function loader() {
-    const contacts = await db.contacts.findMany();
-    return { contacts };
 }
